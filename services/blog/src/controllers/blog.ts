d2fd9d89f1e8dfa093a63e1d95b09590e02b4fa1
Pranav Patil <10dpranav.36921@gmail.com>
2026-04-11 17:45:15 +0530

    feat(blog): add Redis caching with Upstash for getAllBlogs and getSingleBlog endpoints

--- services/blog/src/controllers/blog.ts
+++ services/blog/src/controllers/blog.ts
@@ -1,11 +1,23 @@
+
 import TryCatch from "../utils/TryCatch.js";
 import {sql} from "../utils/db.js"
 import axios from "axios";
+import {redisClient} from "../server.js";
 
+
+//in this redis implemented
 export const getAllBlogs = TryCatch(async(req,res)=>{
 
-    const {searchQuery, category} = req.query;
+    const {searchQuery="", category=""} = req.query;
 
+    const cacheKey= `blogs:${searchQuery}:${category}`;
+    
+    const cached= await redisClient.get(cacheKey)
+    if(cached){
+        console.log("Serving from Redis Cache");
+        res.json(JSON.parse(cached));   //This converts:string → object, as in redis we store as string, correct JSON structure
+        return;
+    }
     let blogs
 
     if(searchQuery && category){
@@ -24,16 +36,37 @@ export const getAllBlogs = TryCatch(async(req,res)=>{
         OR 
         description ILIKE ${"%" + searchQuery + "%"}) 
         ORDER BY createdAt DESC`;
-    }else{
+    }else if(category){
+        blogs= await sql `SELECT * FROM blogs 
+        WHERE 
+        category = ${category}
+        ORDER BY createdAt DESC`;
+    }
+    else{
         blogs= await sql `SELECT * FROM blogs ORDER BY createdAt DESC`;
     }
 
-    
+    console.log("Serving data from DB")
+
+    //once db se data liya toh redisClient mein save kr
+    await redisClient.set(cacheKey, JSON.stringify(blogs),{EX: 3600})
 
     res.json(blogs);
 });
 
 export const getSingleBlog=TryCatch(async(req,res)=>{
+    
+    const blogid= req.params.id;
+    const cacheKey= `blog:${blogid}`
+
+    const cached = await redisClient.get(cacheKey)
+    if(cached){
+        console.log("data coming from Redis ")
+        res.json(JSON.parse(cached))
+        return
+    }
+
+    
     const blog= await sql `SELECT * FROM blogs WHERE id=${req.params.id}`;
 
     
@@ -50,6 +83,11 @@ Client → Blog Service → calls User Service → returns merged data
      const {data} = await axios.get(
         `${process.env.USER_SERVICE}/api/users/user/${blog[0].author}`
     )
+    console.log("Service coming from DB")
+    
+    const responseData= {blog:blog[0],author:data};
+    await redisClient.set(cacheKey, JSON.stringify(responseData), {EX: 10})
+    
     return res.json({
         blog:blog[0],
         author:data
--- services/blog/src/server.ts
+++ services/blog/src/server.ts
@@ -2,10 +2,20 @@ import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 import blogRoutes from "./routes/blog.js";
+import {createClient} from "redis";
 
 const app=express();
 
 const PORT=process.env.PORT 
+
+if(!process.env.REDIS_URL){
+    throw new Error("Redis URL is not defined")
+}
+export const redisClient= createClient({
+    url: process.env.REDIS_URL ,
+});
+
+redisClient.connect().then(()=>console.log("Connected to redis")).catch(console.error);
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
